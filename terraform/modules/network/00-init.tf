b9193a34ddb1fef071423db18fbb4525778761dc
lowflying <chriscm.neu@gmail.com>
2021-06-17 15:31:58 +0100

    change provider var

--- terraform/modules/network/00-init.tf
+++ terraform/modules/network/00-init.tf
@@ -9,7 +9,7 @@ provider "azurerm" {
 }
 
 provider "azurerm" {
-  subscription_id = ${var.log_analytics_subscription_id}
+  subscription_id = var.log_analytics_subscription_id
   alias           = "log-analytics-subscription"
   version         = ">=2.0.0"
   features {}
