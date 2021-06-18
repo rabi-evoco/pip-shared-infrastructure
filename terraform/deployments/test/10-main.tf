d2c93a23adca0e3fd243c20f5517747fac0b1463
lowflying <chriscm.neu@gmail.com>
2021-06-18 15:14:02 +0100

    hardcoded project value for appins

--- terraform/deployments/test/10-main.tf
+++ terraform/deployments/test/10-main.tf
@@ -30,7 +30,7 @@ module "app-insights" {
   environment    = var.environment
   resource_group = var.resource_group
   location       = var.location
-  project        = var.project
+  project        = "pip"
   support_email  = var.support_email
   ping_tests     = var.ping_tests
   tags           = var.tags
