7095688ec20334be6827d8c3cbad0404829b418e
Christopher Pateman <christopher.pateman@version1.com>
2021-09-08 10:13:39 +0100

    PUB-714 update naming

--- terraform/modules/storage-account/main.tf
+++ terraform/modules/storage-account/main.tf
@@ -1,11 +1,10 @@
 
 module "sa" {
   source = "git::https://github.com/hmcts/cnp-module-storage-account.git?ref=master"
-  #source = "../../../cnp-module-storage-account"
 
   env = var.env
 
-  storage_account_name = "hmisharedinfrasa${var.env}"
+  storage_account_name = "pipsharedinfrasa${var.env}"
   common_tags          = var.common_tags
 
   resource_group_name = var.resource_group_name
@@ -16,6 +15,6 @@ module "sa" {
   account_replication_type = var.sa_account_replication_type
   access_tier              = var.sa_access_tier
 
-  team_name    = "HMI DevOps"
+  team_name    = "PIP DevOps"
   team_contact = "#vh-devops"
 }
